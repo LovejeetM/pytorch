404c97bf3eb5a5a9171011c2fb75aba7f49126f4
LovejeetM <lovejeetmatharu54@gmail.com>
2025-07-17 23:13:42 +0530

    tensors

--- tensors.py
+++ tensors.py
@@ -43,3 +43,77 @@ a = tensor2.numpy()
 print(a)
 c = torch.from_numpy(a)
 print(c)
+
+pandas_series=pd.Series([0.1, 2, 0.3, 10.1])
+new_tensor=torch.from_numpy(pandas_series.values)
+print("The new tensor from numpy array: ", new_tensor)
+print("The dtype of new tensor: ", new_tensor.dtype)
+print("The type of new tensor: ", new_tensor.type())
+
+this_tensor=torch.tensor([0,1, 2,3]) 
+
+print("the first item is given by",this_tensor[0].item(),"the first tensor value is given by ",this_tensor[0])
+print("the second item is given by",this_tensor[1].item(),"the second tensor value is given by ",this_tensor[1])
+print("the third  item is given by",this_tensor[2].item(),"the third tensor value is given by ",this_tensor[2])
+
+print(tensor1[1])
+
+torch_to_list=this_tensor.tolist()
+
+print('tensor:', this_tensor,"\nlist:",torch_to_list)
+
+tensor_sample = torch.tensor([20, 1, 2, 3, 4])
+
+subset_tensor_sample = tensor_sample[1:4]
+print("Original tensor sample: ", tensor_sample)
+print("The subset of tensor sample:", subset_tensor_sample)
+
+selected_indexes = [1, 3]
+tensor_sample[selected_indexes] = 100000
+print("Modified tensor with one value: ", tensor_sample)
+
+len_5_tensor = torch.linspace(-2, 2, steps = 5)
+print ("First Try on linspace", len_5_tensor)
+
+len_9_tensor = torch.linspace(-2, 2, steps = 9)
+print ("Second Try on linspace", len_9_tensor)
+
+pi_tensor = torch.linspace(0, 2*np.pi, 100)
+sin_result = torch.sin(pi_tensor)
+
+plt.plot(pi_tensor.numpy(), sin_result.numpy())
+
+twoD_list = [[11, 12, 13], [21, 22, 23], [31, 32, 33]]
+twoD_tensor = torch.tensor(twoD_list)
+print("The New 2D Tensor: ", twoD_tensor)
+
+print("The dimension of twoD_tensor: ", twoD_tensor.ndimension())
+print("The shape of twoD_tensor: ", twoD_tensor.shape)
+print("The shape of twoD_tensor: ", twoD_tensor.size())
+print("The number of elements in twoD_tensor: ", twoD_tensor.numel())
+
+df = pd.DataFrame({'a':[11,21,31],'b':[12,22,312]})
+
+print("Pandas Dataframe to numpy: ", df.values)
+print("Type BEFORE converting: ", df.values.dtype)
+
+new_tensor = torch.from_numpy(df.values)
+print("Tensor AFTER converting: ", new_tensor)
+print("Type AFTER converting: ", new_tensor.dtype)
+
+
+X = torch.tensor([[1, 0],[0, 1]]) 
+Y = torch.tensor([[2, 1],[1, 2]])
+X_plus_Y = X + Y
+print("The result of X + Y: ", X_plus_Y)
+
+X = torch.tensor([[1, 0], [0, 1]])
+Y = torch.tensor([[2, 1], [1, 2]]) 
+X_times_Y = X * Y
+print("The result of X * Y: ", X_times_Y)
+
+A = torch.tensor([[0, 1, 1], [1, 0, 1]])
+B = torch.tensor([[1, 1], [1, 1], [-1, 1]])
+A_times_B = torch.mm(A,B)
+print("The result of A * B: ", A_times_B)
+
